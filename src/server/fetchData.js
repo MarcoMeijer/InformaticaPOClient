23792bbf972e7bf1fdd8d98d23741f76d9765e34
MarcoMeijer <46689298+MarcoMeijer@users.noreply.github.com>
2020-12-15 19:45:14 +0100

    updated server url

--- src/server/fetchData.js
+++ src/server/fetchData.js
@@ -1,12 +1,13 @@
 
-const serverURL = 'http://localhost:4000';
+const serverURL = 'http://94.214.161.27:4000';
 
-export default function getData(url, body) {
-  return fetch(`${serverURL}/${url}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: body
-  }).then(response => response.json());
+export default async function getData(url, body) {
+  const response = await fetch(`${serverURL}/${url}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: body
+    });
+    return await response.json();
 }
