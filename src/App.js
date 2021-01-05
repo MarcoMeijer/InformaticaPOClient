2ea96c361ffa6fbb1737ee54559150bd1ad47d5d
MarcoMeijer <46689298+MarcoMeijer@users.noreply.github.com>
2021-01-05 19:29:16 +0100

    Fixed tabs

--- src/App.js
+++ src/App.js
@@ -4,25 +4,25 @@ import * as React from 'react';
 import { Button, Text, View } from 'react-native';
 import RadioChoices from './Gui/RadioChoices';
 
-function HomeScreen({navigation}) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
-            <Button
-                title="Go to Test screen"
-                onPress={() => navigation.navigate('Test')}
-            />
-        </View>
-    );
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Test screen"
+        onPress={() => navigation.navigate('Test')}
+      />
+    </View>
+  );
 }
-function TestScreen({navigation}) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Test Screen</Text>
-            <RadioChoices options={["Optie 1", "Optie 2", "Optie 3"]}/>
-            <Button title="Go back" onPress={() => navigation.goBack()} />
-        </View>
-    );
+function TestScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Test Screen</Text>
+      <RadioChoices options={["Optie 1", "Optie 2", "Optie 3"]} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
 }
 
 const Stack = createStackNavigator();
@@ -30,12 +30,12 @@ const Stack = createStackNavigator();
 export default class App extends React.Component {
   render() {
     return (
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Test" component={TestScreen} />
-          </Stack.Navigator>
-        </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Test" component={TestScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
