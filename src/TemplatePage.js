1127d237521d68f4954fc32bc4ece2a2a63021a1
MarcoMeijer <46689298+MarcoMeijer@users.noreply.github.com>
2021-01-12 18:33:58 +0100

    Navigatie bij template

--- src/TemplatePage.js
+++ src/TemplatePage.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { View } from 'react-native';
 
-export default function TEMPLATE() {
+export default function TEMPLATE({ navigation }) {
   return (
     <View>
 
