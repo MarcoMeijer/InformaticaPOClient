457509d5f896c615e3343a77d152cbdbbc4a0865
118914 Rodenboog <118914@gomaruscollege.nl>
2021-01-19 12:54:56 +0100

    no changes

--- src/BartTest1.js
+++ src/BartTest1.js
@@ -1,10 +1,21 @@
+import Editor from "@monaco-editor/react";
 import * as React from 'react';
-import { View } from 'react-native';
+import { Button, Text, TextInput, View } from 'react-native';
+import ExamTextComp from './Gui/ExamTextComp';
+import { styles } from './Styles';
 
 export default function BartTest1() {
+  const [llnr, changellnr] = React.useState('leerlingnummer');
+  const [password, changepassword] = React.useState('');
   return (
     <View>
-
+      <Text style={styles.text}>
+          Voer hier je leerlingnummer in:
+      </Text>
+      <TextInput
+          style={styles.textBox}
+          onChangeText={text => changellnr(text)}
+        />
     </View>
   );
 }
