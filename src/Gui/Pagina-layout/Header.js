27c1b5330f0dadfc979b067753c210059adbf027
CodeBoomBert <76482028+CodeBoomBert@users.noreply.github.com>
2021-03-23 16:57:01 +0100

    Header update and statestiek

--- src/Gui/Pagina-layout/Header.js
+++ src/Gui/Pagina-layout/Header.js
@@ -1,5 +1,5 @@
 import { useTheme } from "@react-navigation/native";
-
+import { useState } from "react";
 import * as React from "react";
 import { View, TouchableOpacity, Image } from "react-native";
 import Button from "../Basic/Button";
@@ -9,76 +9,120 @@ import pullDownPng from "../../afbeeldingen/pulldownknop.png";
 
 export default function Header({ navigation, children }) {
   const { colors, addSucces, windowWidth } = useTheme();
-
+  const [open, setOpen] = useState(false);
   const afbeeldingGrote = 0.13;
 
-  if (windowWidth >= 625) {
-    return (
-      <View
-        style={{
-          flexDirection: "row",
-          backgroundColor: colors.headerKleur,
-          borderBottomWidth: 1,
-          borderBottomColor: colors.headerLijnKleur,
-          alignItems: "center"
-        }}
-      >
-        <View style={{ flexDirection: "row", flex: 1 }}>
-          <View
-            style={{
-              marginLeft: 20,
-              margin: 6
-            }}
-          >
-            <Logo size={0.25} />
+  let OpenHeader = () => {
+    if (windowWidth >= 625) {
+      return (
+        <View
+          style={{
+            flexDirection: "row",
+            backgroundColor: colors.headerKleur,
+            borderBottomWidth: 1,
+            borderBottomColor: colors.headerLijnKleur,
+            alignItems: "center"
+          }}
+        >
+          <View style={{ flexDirection: "row", flex: 1 }}>
+            <View
+              style={{
+                marginLeft: 20,
+                margin: 6
+              }}
+            >
+              <Logo size={0.25} />
+            </View>
+
+            {children}
           </View>
 
-          {children}
+          <DarkModeSwitch style={{ margin: 10 }} />
+          <Button
+            style={{ margin: 10 }}
+            title="Uitloggen"
+            onPress={() => {
+              navigation.navigate("Home");
+              addSucces("U bent succesvol uitgelogd.");
+            }}
+          ></Button>
         </View>
-
-        <DarkModeSwitch style={{ margin: 10 }} />
-        <Button
-          style={{ margin: 10 }}
-          title="Uitloggen"
-          onPress={() => {
-            navigation.navigate("Home");
-            addSucces("U bent succesvol uitgelogd.");
+      );
+    } else {
+      return (
+        <View
+          style={{
+            flexDirection: "row",
+            backgroundColor: colors.headerKleur,
+            borderBottomWidth: 1,
+            borderBottomColor: colors.headerLijnKleur,
+            alignItems: "center"
           }}
-        ></Button>
+        >
+          <View style={{ flexDirection: "row", flex: 1 }}>
+            <View
+              style={{
+                marginLeft: 20,
+                margin: 6
+              }}
+            >
+              <Logo size={0.25} />
+            </View>
+          </View>
+
+          <TouchableOpacity onPress={() => setOpen(!open)}>
+            <Image
+              style={{
+                marginRight: 15,
+                width: afbeeldingGrote * 478,
+                height: afbeeldingGrote * 341
+              }}
+              source={pullDownPng}
+            />
+          </TouchableOpacity>
+        </View>
+      );
+    }
+  };
+
+  if (windowWidth <= 625) {
+    return (
+      <View style={{ zIndex: 2 }}>
+        <OpenHeader />
+        {open && (
+          <View style={{ position: "absolute", flex: 1, width: "100%" }}>
+            <OpenHeader />
+            <View
+              style={{
+                backgroundColor: colors.headerKleur,
+                borderBottomColor: colors.headerLijnKleur,
+                borderBottomWidth: 1
+              }}
+            >
+              {children}
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignSelf: "center",
+                  marginBottom: 4
+                }}
+              >
+                <DarkModeSwitch style={{ margin: 10 }} />
+                <Button
+                  style={{ width: 80, height: 10 }}
+                  title="Uitloggen"
+                  onPress={() => {
+                    navigation.navigate("Home");
+                    addSucces("U bent succesvol uitgelogd.");
+                  }}
+                ></Button>
+              </View>
+            </View>
+          </View>
+        )}
       </View>
     );
   } else {
-    return (
-      <View
-        style={{
-          flexDirection: "row",
-          backgroundColor: colors.headerKleur,
-          borderBottomWidth: 1,
-          borderBottomColor: colors.headerLijnKleur,
-          alignItems: "center"
-        }}
-      >
-        <View style={{ flexDirection: "row", flex: 1 }}>
-          <View
-            style={{
-              marginLeft: 20,
-              margin: 6
-            }}
-          >
-            <Logo size={0.25} />
-          </View>
-        </View>
-        <TouchableOpacity onPress={() => {}}>
-          <Image
-            style={{
-              marginRight
-              width: afbeeldingGrote * 478,
-              height: afbeeldingGrote * 341
-            }}
-            source={pullDownPng}
-          />
-        </TouchableOpacity>
-      </View>
-    );
+    return <OpenHeader />;
   }
 }
--- src/Paginas/leerlingen/leerlingStatistiekPagina.js
+++ src/Paginas/leerlingen/leerlingStatistiekPagina.js
@@ -1,12 +1,20 @@
 import * as React from "react";
-import { ActivityIndicator, View } from "react-native";
+import { ActivityIndicator, View, TouchableOpacity } from "react-native";
 import { VictoryPie } from "victory";
 import Text from "../../Gui/Basic/Text";
 import Jacket from "../../Gui/Pagina-layout/Jacket";
 import useFetch from "../../Hooks/useFetch";
+import TekstenLijst from "../../Gui/ExamenTekst/TekstenLijst";
+import Button from "../../Gui/Basic/Button";
+import { useTheme } from "@react-navigation/native";
+import { useEffect, useState } from "react";
 
-export default function LeerlingStatistiekPagina() {
+export default function LeerlingStatistiekPagina({ navigation }) {
   const [gemaakt] = useFetch("gemaakt");
+  const [examens] = useFetch("examens");
+  const { colors } = useTheme();
+  const [open, zetOpen] = useState(false);
+  const [teksten, zetTeksten] = useState(undefined);
 
   let punten = 0;
   let maximaalPunten = 0;
@@ -19,6 +27,38 @@ export default function LeerlingStatistiekPagina() {
 
   return (
     <Jacket>
+      <Button
+        title="temp"
+        style={{ margin: 5 }}
+        onPress={() => zetOpen(!open)}
+      />
+      {open &&
+        teksten !== undefined &&
+        teksten.map((tekst, index) => {
+          return (
+            <View
+              key={index}
+              style={{
+                backgroundColor: colors.textboxAchtergrondKleur,
+                borderRadius: 3,
+                borderWidth: 1,
+                borderColor: colors.textboxAchtergrondKleur,
+                marginLeft: 5,
+                marginRight: 5,
+                marginBottom: 2
+              }}
+            >
+              <Text
+                style={{
+                  color: colors.tekstKleur
+                }}
+                onPress={() => onPress(tekst.tekstid)}
+              >
+                {tekst.title}
+              </Text>
+            </View>
+          );
+        })}
       {gemaakt === undefined ? (
         <ActivityIndicator />
       ) : (
