2b2b31e91137af2189fad30b01a72ead6aad7af2
MarcoMeijer <46689298+MarcoMeijer@users.noreply.github.com>
2021-03-26 22:47:50 +0100

    fdgdf

--- src/Gui/ExamenTekst/TekstenLijst.js
+++ src/Gui/ExamenTekst/TekstenLijst.js
@@ -7,27 +7,38 @@ import FouwDoos from "../Basic/FouwDoos";
 import { ToevoegenKnop } from "../Basic/Knoppen";
 import ExamenTekstSelecteerder from "./ExamenTekstSelecteerder";
 
-export default function TekstenLijst({ onPress, onExamenToevoegen, onEditExamen, onTekstToevoegen, onVerwijderExamen, onTekstVerwijder }) {
+export default function TekstenLijst({
+  onPress,
+  onExamenToevoegen,
+  onEditExamen,
+  onTekstToevoegen,
+  onVerwijderExamen,
+  onTekstVerwijder
+}) {
   const [open, zetOpen] = useState(false);
   const [examens, updateExamens] = useFetch("examens");
-  const {colors} = useTheme();
+  const { colors } = useTheme();
 
   return (
     <View style={{ alignSelf: "stretch" }}>
-      {
-        examens === undefined
-        ? <ActivityIndicator/>
-        : examens.map(({ examennaam }) => {
+      {examens === undefined ? (
+        <ActivityIndicator />
+      ) : (
+        examens.map(({ examennaam }) => {
           return (
             <FouwDoos
               titel={examennaam}
               key={examennaam}
               lazy={true}
               onEdit={onEditExamen}
-              onDelete={onVerwijderExamen && (() => {
-                return onVerwijderExamen(examennaam)
-                .then(() => updateExamens())
-              })}
+              onDelete={
+                onVerwijderExamen &&
+                (() => {
+                  return onVerwijderExamen(examennaam).then(() =>
+                    updateExamens()
+                  );
+                })
+              }
             >
               <ExamenTekstSelecteerder
                 examennaam={examennaam}
@@ -38,30 +49,27 @@ export default function TekstenLijst({ onPress, onExamenToevoegen, onEditExamen,
             </FouwDoos>
           );
         })
-      }
-      {
-        onExamenToevoegen &&
+      )}
+      {onExamenToevoegen && (
         <View>
-          {
-            open &&
+          {open && (
             <ExamenAanmakenMenu
               onCreate={(examenNaam) => {
-                onExamenToevoegen(examenNaam)
-                  .then(() => {
-                    zetOpen(false);
-                    updateExamens();
-                  })
+                onExamenToevoegen(examenNaam).then(() => {
+                  zetOpen(false);
+                  updateExamens();
+                });
               }}
             />
-          }
+          )}
           <ToevoegenKnop
-            style={{margin: 5, alignSelf: "center"}}
+            style={{ margin: 5, alignSelf: "center" }}
             title="Nieuw examen"
             size={20}
             onPress={() => zetOpen(!open)}
           />
         </View>
-      }
+      )}
     </View>
   );
 }
